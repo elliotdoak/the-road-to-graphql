ec488b24265953a45e1d3605e69985020769ed63
ElliotDoak <elliot.doak@atos.net>
2022-05-17 09:26:13 +0100

    Install apollo and graphql, set up github personal access token env variable

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import 'dotenv/config';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: (operation) => {
+    headers: {
+      authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`;
+    }
+  },
+});
 
 const userCredentials = { firstname: 'Elliot' };
 const userDetails = { nationality: 'Scottish' };
