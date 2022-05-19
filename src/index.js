79a9fa3e9670e1f14138de0f9866122325b39ee4
ElliotDoak <elliot.doak@atos.net>
2022-05-19 09:24:36 +0100

    Add apollo query and install several packages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 import 'dotenv/config';
-import ApolloClient from 'apollo-boost';
+import 'cross-fetch/polyfill';
+import ApolloClient, { gql } from 'apollo-boost';
 
 const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
   request: (operation) => {
-    headers: {
-      authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`;
-    }
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+      },
+    });
   },
 });
+
+const GET_REPO_OF_ORG = gql`
+  query ($org: String!) {
+    organization(login: $org) {
+      name
+      url
+      repositories(first: 5) {
+        edges {
+          node {
+            name
+            url
+          }
+        }
+      }
+    }
+  }
+`;
+
+client
+  .query({
+    query: GET_REPO_OF_ORG,
+    variables: { org: 'the-road-to-learn-react' },
+  })
+  .then(console.log);
 
 const userCredentials = { firstname: 'Elliot' };
 const userDetails = { nationality: 'Scottish' };
@@ -18,6 +45,6 @@ const user = {
   ...userDetails,
 };
 
-console.log(user);
+// console.log(user);
 
-console.log(process.env.SOME_ENV_VARIABLE);
+// console.log(process.env.SOME_ENV_VARIABLE);
